0a2ca30f06b8c160673af8d9f2952b26601649c0
dikshitbhatta <147680213+dikshitbhatta@users.noreply.github.com>
2024-09-01 14:09:55 +0545

    .

--- bottomnavbar/lib/widget/leftwidget.dart
+++ bottomnavbar/lib/widget/leftwidget.dart
@@ -1,4 +1,5 @@
 import 'package:bottomnavbar/widget/menu_list_tile.dart';
+import 'package:bottomnavbar/Pages/reminder.dart';
 import 'package:flutter/material.dart';
 
 class Leftwidget extends StatelessWidget {
@@ -22,7 +23,15 @@ class Leftwidget extends StatelessWidget {
               color: Colors.white,
             ),
             otherAccountsPictures: <Widget>[
-              Icon(Icons.bookmark_outline),
+              IconButton(
+                icon: Icon(Icons.bookmark_outline,
+                    color: Colors.white, fill: 1.00),
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Reminder()));
+                },
+              ),
             ],
             decoration: BoxDecoration(
               image: DecorationImage(
