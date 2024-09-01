40e99a02130ac0d7eeafd28315678c1e7037e5ce
dikshitbhatta <147680213+dikshitbhatta@users.noreply.github.com>
2024-09-02 00:23:52 +0545

    Gridview extent

--- bottomnavbar/lib/Pages/graditude.dart
+++ bottomnavbar/lib/Pages/graditude.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:bottomnavbar/methods/gridicon.dart';
 
 class Graditude extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<IconData> _iconList = GridIcons().getIconList();
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(20.00),
-        child: Icon(
-          Icons.sentiment_satisfied,
-          size: 100.00,
-        ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        padding: EdgeInsets.all(8.00),
+        children: List.generate(70, (index) {
+          print('_buildGridview $index');
+          return Card(
+            margin: EdgeInsets.all(8.00),
+            child: InkWell(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    _iconList[0],
+                    size: 48.00,
+                    color: Colors.blue,
+                  ),
+                  Divider(),
+                  Text(
+                    'Index $index',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16.00),
+                  )
+                ],
+              ),
+              onTap: () {
+                print('Row $index');
+              },
+            ),
+          );
+        }),
       ),
     );
   }
--- bottomnavbar/lib/Pages/reminder.dart
+++ bottomnavbar/lib/Pages/reminder.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:bottomnavbar/methods/gridicon.dart';
 
 class Reminder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Padding(
-      padding: EdgeInsets.all(20.00),
-      child: Icon(
-        Icons.access_alarm,
-        size: 100.00,
-      ),
-    ));
+    List<IconData> _iconList = GridIcons().getIconList();
+
+    return GridView.extent(
+      maxCrossAxisExtent: 175.00,
+      scrollDirection: Axis.horizontal,
+      padding: EdgeInsets.all(8.0),
+      children: List.generate(20, (index) {
+        print('_buildgridviewextene $index');
+        return Card(
+          margin: EdgeInsets.all(8.00),
+          child: InkWell(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  _iconList[index],
+                  color: Colors.blue,
+                  size: 20,
+                ),
+                Divider(),
+                Text('Photo $index'),
+              ],
+            ),
+            onTap: () {
+              print('Row $index');
+            },
+          ),
+        );
+      }),
+    );
   }
 }
