0c98b5ed4512605efd8197a9a5237e39b2905906
dikshitbhatta <147680213+dikshitbhatta@users.noreply.github.com>
2024-09-01 22:40:27 +0545

    ListTile

--- listandviews/lib/Pages/listview.dart
+++ listandviews/lib/Pages/listview.dart
@@ -8,6 +8,61 @@ class Listview extends StatefulWidget {
 class _ListviewState extends State<Listview> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Card(
+            elevation: 5.00,
+            shape: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.00),
+                borderSide: BorderSide(color: Colors.blue)),
+            child: ListTile(
+              leading: Icon(Icons.flight),
+              title: Text('Airplane 1'),
+              trailing: Text(
+                '10%',
+                style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 10.00,
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                'cool',
+                style: TextStyle(color: Colors.grey),
+              ),
+            ),
+          ),
+          Card(
+            elevation: 5.00,
+            shape: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.00),
+                borderSide: BorderSide(color: Colors.blue)),
+            child: ListTile(
+              leading: Icon(Icons.flight),
+              title: Text('Airplane 2'),
+              trailing: Text(
+                '10%',
+                style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 10.00,
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                'cool',
+                style: TextStyle(color: Colors.grey),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.car_repair),
+            title: Text(
+              'Car',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text('Car for rent'),
+          )
+        ],
+      ),
+    );
   }
 }
